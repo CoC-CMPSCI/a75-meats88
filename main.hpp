075fc9b52fd11f16f86633b11b94e40640d480d8
meats88 <pcx883mi@gmail.com>
2026-06-03 20:25:47 -0700

    finish

--- main.hpp
+++ main.hpp
@@ -11,7 +11,22 @@ int finduserstring(char[], int, char[], int);
 
 int finduserstring(char cstr[], int cstrlen, char userstr[], int userlen)
 {
-    // TODO: scan cstr for the first occurrence of userstr, return its starting
+    if (userlen > cstrlen) {
+        return -1;
+    }
+    for (int i = 0; i <= cstrlen - userlen; i++) {
+        bool match = true;
+        for (int j = 0; j < userlen; j++) {
+            if (cstr[i + j] != userstr[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 #endif
